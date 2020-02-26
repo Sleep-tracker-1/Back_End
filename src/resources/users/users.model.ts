db4f20a9f4156c6c119fe5a151fbaacff30f74ff
Westley Feller <feller.westley@gmail.com>
2020-02-26 10:21:38 -0800

    Users model

---
+++ src/resources/users/users.model.ts
@@ -0,0 +1,36 @@
+import { QueryBuilder } from 'knex'
+
+import { Id } from '../../utils/crud'
+import db from '../../data/dbConfig'
+
+export type User = {
+  id: Id
+  username: string
+  password: string
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const findBy = (filter: { [key: string]: any }): QueryBuilder =>
+  db('users')
+    .select('*')
+    .where(filter)
+
+export const findById = (id: Id): QueryBuilder<User> =>
+  db('users')
+    .where({ id: Number(id) })
+    .first()
+
+export const insert = (user: Omit<User, 'id'>): QueryBuilder<[User]> =>
+  db('users').insert(user, ['id', 'username'])
+
+export const update = (id: Id, user: Omit<User, 'id'>): QueryBuilder<User> =>
+  db('users')
+    .where({ id: Number(id) })
+    .update(user, ['id', 'username'])
+
+export const remove = (id: Id): QueryBuilder<number> =>
+  db('users')
+    .where('id', Number(id))
+    .del()
+
+export default { findById, insert, update, remove }
