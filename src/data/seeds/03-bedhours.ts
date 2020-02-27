82848b23cfc2ce1c022693bbd54973c7aa9002a5
Westley Feller <feller.westley@gmail.com>
2020-02-26 22:32:09 -0800

    User seed and improved datetime seed

--- src/data/seeds/03-bedhours.ts
+++ src/data/seeds/03-bedhours.ts
@@ -1,19 +1,21 @@
 import Knex, { QueryBuilder } from 'knex'
 import { add } from 'date-fns'
 
-const generateTimestamps = () => {
-  return [...Array(7)].map((_id, userIndex) => {
-    const startDate = new Date(
-      2019,
-      2,
-      25,
-      Math.floor(Math.random() * 23) + 18,
-      Math.random() * 60,
-      Math.random() * 60
-    )
+const generateTimestamps = (): {
+  user_id: number
+  bedtime: Date
+  waketime: Date
+}[][] => {
+  return [...Array(458)].map((_id, userIndex) => {
+    const startDate = new Date(2019, 2, 25, 19)
 
     return [...Array(365)].map((_days, dateIndex) => {
-      const bedtime = add(startDate, { days: dateIndex })
+      const day = add(startDate, { days: dateIndex })
+      const bedtime = add(day, {
+        hours: Math.floor(Math.random() * 7),
+        minutes: Math.random() * 60,
+        seconds: Math.random() * 60,
+      })
       const waketime = add(bedtime, { hours: Math.random() * (12 - 4) + 4 })
 
       return {
