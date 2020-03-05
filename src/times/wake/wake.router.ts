3f9f60b7be746f90e72c1025289177cffa8478bd
Westley Feller <feller.westley@gmail.com>
2020-03-05 13:34:33 -0800

    Validation for wake, midday, night

--- src/times/wake/wake.router.ts
+++ src/times/wake/wake.router.ts
@@ -2,9 +2,10 @@ import { Router } from 'express'
 
 import addWake from './wake.controllers'
 import checkAuth from '../../auth/middleware/checkAuth'
+import validate from './middleware/validation'
 
 const router = Router()
 
-router.route('/').put(checkAuth, addWake)
+router.route('/').put(checkAuth, validate, addWake)
 
 export default router
