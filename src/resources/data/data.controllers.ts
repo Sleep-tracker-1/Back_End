a0217b5fe25bb2a59e5f59d8512e7e33a07fcfe6
Westley Feller <feller.westley@gmail.com>
2020-03-06 00:26:12 -0800

    Using DS script

--- src/resources/data/data.controllers.ts
+++ src/resources/data/data.controllers.ts
@@ -1,16 +1,17 @@
 import { NextFunction, Response } from 'express'
 import { differenceInMinutes, format } from 'date-fns'
-import { AuthorizationRequest } from '../../auth/middleware/checkAuth'
 import { findById } from '../users/users.model'
 import { getAllData, SleepData } from './data.model'
 import { DatabaseError } from '../../server/middleware/errorHandler'
+import { RequestWithData } from './middleware/sleepData'
 
 const getData = async (
-  req: AuthorizationRequest,
+  req: RequestWithData,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   try {
+    const averages = req.sleepData.match(/\d+\.+\d+/g).map(Number)
     const start = req.query.start
       ? req.query.start.split('-')
       : '1-1-1900'.split('-')
@@ -27,6 +28,10 @@ const getData = async (
       return {
         date: format(night.waketime, 'M/d/yy'),
         dateId: night.dateId,
+        sleepRecommendation: averages[0],
+        averageHoursOfSleep: averages[1],
+        averageMood: averages[2],
+        averageTiredness: averages[3],
         totalTimeInBed: (
           differenceInMinutes(night.waketime, night.bedtime) / 60
         ).toFixed(1),
---
+++ src/resources/data/middleware/sleepData.ts
@@ -0,0 +1,35 @@
+import { Response, NextFunction } from 'express'
+import { PythonShell } from 'python-shell'
+import { sub } from 'date-fns'
+import { AuthorizationRequest } from '../../../auth/middleware/checkAuth'
+import { find } from '../../bedhours/bedhours.model'
+
+export type RequestWithData = AuthorizationRequest & {
+  sleepData?: any
+}
+
+export const calculateSleepData = async (
+  req: RequestWithData,
+  _res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const options = {
+    pythonOptions: ['-u'],
+    args: [`${req.decodedJwt!.username}`],
+  }
+
+  const bedHours = await find(sub(new Date(), { years: 100 }), new Date())
+
+  if (bedHours.length > 30) {
+    PythonShell.run('/dataScience/analysis.py', options, (error, results) => {
+      if (error) {
+        next(error)
+      } else {
+        req.sleepData = [results]
+        next()
+      }
+    })
+  } else {
+    next()
+  }
+}
